0ca697a4bbc09d9fa7ba59a5367f9cc9111b2466
ZioZvevo <99705790+ZioZvevo@users.noreply.github.com>
2022-05-29 11:45:46 -0700

    Add files via upload
    As you can see, there is a rm -rf command: I purposely commented this out and added a "1" in the middle so that we are all sure that that will not actually execute, as that would be disastrous. Please, use this for educational purposes only :)

---
+++ Myfirstbash.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+enter=0
+echo "This following script will delete all files in android."
+sleep 2
+echo  
+
+read -p "[Press ENTER to continue]" 
+enter=1
+sleep 3
+
+for i in 1 .. 2
+ do
+echo 
+ done
+ 
+if [ enter=1 ]
+then
+   echo Press CTRL C to abort
+   echo 
+   sleep 1
+   echo Press CTRL C to abort
+   echo 
+   sleep 1
+   echo Press CTRL C to abort
+fi
+
+for i in 1 .. 2
+ do
+echo 
+ done
+#r1m -rf /storage/emulated/0
+
+echo "Successfully deleted android data"
